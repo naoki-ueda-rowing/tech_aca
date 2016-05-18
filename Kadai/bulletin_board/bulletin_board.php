b9a80af205f2acc6e72cb895117bfe5b18e437c1
naoki-ueda-rowing <naoki826divide@gmail.com>
2016-05-18 18:15:56 +0900

    2016/05/18

--- Kadai/bulletin_board/bulletin_board.php
+++ Kadai/bulletin_board/bulletin_board.php
@@ -26,54 +26,49 @@ require_once 'Encode.php';
 
 <!--POSTデータをデータベースに登録-->
     <?php
-    try{
+    try {
         //データベースの接続を確立
         $db = getDb();
-        //すでにデータベースにデータが入っている場合はそれを表示する
-//        $stt = $db->prepare('SELECT * FROM bulletinboard /*ORDER BY text DESC*/');
-//        if($db!=NULL) {
-//            $stt->execute();
-//            //結果を出力
-//            while ($row = $stt->fetch(PDO::FETCH_ASSOC)) {
-//                ?>
-<!--                <tr>-->
-<!--                    <td>--><?php //e($row['user_name']); ?><!--</td>-->
-<!--                    <br>-->
-<!--                    <td>--><?php //e($row['title']); ?><!--</td>-->
-<!--                    <br>-->
-<!--                    <td>--><?php //e($row['contents']); ?><!--</td>-->
-<!--                    <br><br>-->
-<!--                </tr>-->
-<!--                --><?php
-//            }
-//        }
         //INSERT命令の準備
         $stt = $db->prepare('INSERT INTO  bulletinboard( user_name, title , contents ) VALUES (:user_name, :title, :contents)');
         //INSERT命令にポストデータの内容をセット
-        $stt -> bindValue(':user_name', $_POST['user_name']);
-        $stt -> bindValue(':title', $_POST['title']);
-        $stt -> bindValue(':contents', $_POST['contents']);
+    if (isset($_POST['user_name']) && isset($_POST['title']) && isset ($_POST['contents'])) {
+        $stt->bindValue(':user_name', $_POST['user_name']);
+        $stt->bindValue(':title', $_POST['title']);
+        $stt->bindValue(':contents', $_POST['contents']);
         //INSERT命令を実行
-        $stt -> execute();
-        $stt=NULL;
+        $stt->execute();
+        $stt = NULL;
+    }
+
         $stt = $db->prepare('SELECT * FROM bulletinboard /*ORDER BY text DESC*/');
         $stt->execute();
-        //結果を出力
-        while($row = $stt->fetch(PDO::FETCH_ASSOC)){
-            ?>
-            <table style="width: 200px; border: 1px;solid #ff00ff;">
 
-                <tr><td>名前：　<?php e($row['user_name']); ?><br></td></tr>
-                <tr><td>題名：　<?php e($row['title']); ?><br></td></tr>
-                <tr><td>内容：　<?php e($row['contents']); ?></td></tr><br><br>
 
+            //結果を出力
+            while ($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+
+                <table border="1" width="300" height="300">
+                    <td height="30" width="50"> 名前</td>
+                    <td height="30"> <?php e($row['user_name']); ?></td>
+                    <tr>
+                        <td height="30" width="50"> 題名</td>
+                        <td height="30"><?php e($row['title']); ?></td>
+                    <tr>
+                        <td height="240" width="50"> 本文</td>
+                        <td height="240"><?php e($row['contents']); ?></td>
                 </table>
-            <?php
-        }
-        $db = NULL;
-   } catch(PDOException $e) {
-        die("エラーメッセージ：{$e->getMessage()}");
+                <br><br>
+                <?php
+            }
+            $db = NULL;
+
     }
+    catch
+        (PDOException $e) {
+            die("エラーメッセージ：{$e->getMessage()}");
+        }
     ?>
 </body>
 </html>
