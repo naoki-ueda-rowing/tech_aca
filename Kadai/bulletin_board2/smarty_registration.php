2e01f3924d69ad9c78e14e5316fc3644f13da324
naoki-ueda-rowing <naoki826divide@gmail.com>
2016-06-04 21:25:26 +0900

    2016/06/04

--- Kadai/bulletin_board2/smarty_registration.php
+++ Kadai/bulletin_board2/smarty_registration.php
@@ -20,21 +20,35 @@ try {
 
         //空白の場合は取り除く
         if($_POST['user_id'] != "" &&  $_POST['user_name'] != "" && $_POST['password'] != "" ) {
-            $stt = $db->prepare('INSERT INTO  member( id , name , password ) VALUES (:user_id, :user_name, :password )');
-            $stt->bindValue(':user_id', $_POST['user_id']);
-            $stt->bindValue(':user_name', $_POST['user_name']);
-            $stt->bindValue(':password', $_POST['password']);
-//INSERT命令を実行
-            $stt->execute();
-            $stt = NULL;
-            $db  = NULL;
-            header("Location: smarty_board.php");
-        }
 
+            //ユーザーIDの重複を確認
+            $stt = $db->prepare("SELECT * FROM member WHERE id = '$_POST[user_id]' ");
+            $stt->execute();
+            //user_idが取り出されたかどうか確認
+            $user_id = $stt->rowCount();
+
+            //重複の場合は除く
+            if($user_id == 0) {
+                $stt = $db->prepare('INSERT INTO  member( id , name , password ) VALUES (:user_id, :user_name, :password )');
+                $stt->bindValue(':user_id', $_POST['user_id']);
+                $stt->bindValue(':user_name', $_POST['user_name']);
+                $stt->bindValue(':password', $_POST['password']);
+                //INSERT命令を実行
+                $stt->execute();
+                $stt = NULL;
+                $db = NULL;
+                header("Location: smarty_board.php");
+            }
+            else{
+                "入力エラー：ユーザーIDが重複しています";
+            }
+
+        }
         else{
             print "入力エラー：漏れがあります";
         }
 
+        
     }
 
 
